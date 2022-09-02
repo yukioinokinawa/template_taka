4109cadf5cb2199eb58a3c00abf0c59f6a9b2968
Taka <yukioinokinawa@gmail.com>
2022-09-02 11:14:38 +0900

    tidy up index.php

--- index.php
+++ index.php
@@ -1,57 +1,6 @@
 <?php get_header(); ?>
 <!--Header-->
 
-<div class="p-maintop">
-    <div class="p-maintop__wrapper">
-        <p>ダミーサイト</p>
-    </div>
-</div>
-<!--Tophead-->
-
-<div class="l-main">
-    <div class="p-mainblock__wrapper">
-        <article class="p-mainblock c-backimg1">
-            <h1>Take Out</h1>
-            <div class="p-mainblock__a-wrapper">
-                <article class="p-mainblock__inner">
-                    <h2>Take Out</h2>
-                    <p>当店のテイクアウトで利用できる商品を掲載しています当店のテイクアウトで利用できる商品を掲載しています</p>
-                </article>
-                <article class="p-mainblock__inner">
-                    <h2>Take Out</h2>
-                    <p>当店のテイクアウトで利用できる商品を掲載しています当店のテイクアウトで利用できる商品を掲載しています</p>
-                </article>
-            </div>
-        </article>
-        <article class="p-mainblock c-backimg2">
-            <h1>Eat In</h1>
-            <div class="p-mainblock__a-wrapper">
-                <article class="p-mainblock__inner">
-                    <h2>Eat In</h2>
-                    <p>店内でお食事いただけるメニューです店内でお食事いただけるメニューです 店内でお食事いただけるメニューです店内でお食事いただけるメニューです</p>
-                </article>
-                <article class="p-mainblock__inner">
-                    <h2>Eat In</h2>
-                    <p>店内でお食事いただけるメニューです店内でお食事いただけるメニューです </p>
-                </article>
-            </div>
-        </article>
-    </div>
-</div>
-<!--Main-->
-
-<div class="c-map">
-    <div class="c-map__wrapper">
-        <div class="c-map__shade"></div>
-        <article class="c-map__textarea">
-            <h1>見出しが入ります</h1>
-            <p>テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。
-            </p>
-        </article>
-    </div>
-</div>
-<!--Map-->
-</div>
 
 <?php get_sidebar(); ?>
 
